a90200a24e4dd4b7fbcd288504b37cb679814539
escherer <emerson@emersonscherer.com>
2015-05-06 21:58:50 +0000

    Added Edit and Update Views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,5 +7,43 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+  
+  def new
+    @recipe = Recipe.new
+  end
+  
+  def create
+    @recipe = Recipe.new(recipe_params)
+    #hardcoding chef for development until user authentication is added
+    @recipe.chef = Chef.find(2)
+    
+    if @recipe.save
+      flash[:sucess] = "Your recipe was created successfully."
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def update
+    @recipe = Recipe.find(params[:id])
+      if @recipe.update(recipe_params)
+        #do something
+        flash[:success] = "Your recipe was updated successfully!"
+        redirect_to recipe_path(@recipe)
+      else
+        render :edit
+      end
+  end
+  
+  private
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
+  
 
 end
